3205cd6dc1bd4bd13df0002ce100f6c67aa645f9
vipdanger <vip.danger@mail.ru>
2024-04-03 03:26:48 +0300

    1

--- Algoritms/Output/out.go
+++ Algoritms/Output/out.go
@@ -2,16 +2,36 @@ package Output
 
 import (
 	"fmt"
+	"os"
 	"strings"
 )
 
 var Str string
+var filename = "Algoritms/Output/buffer.txt"
 
 func Scan(a ...any) (n int, err error) {
-	reader := strings.NewReader(Str)
-	n, err = fmt.Fscanln(reader, a)
+	for _, i := range a {
+		reader := strings.NewReader(Str)
+		n, err = fmt.Fscan(reader, i)
+		fmt.Fscan(reader, i)
+		Str = strings.TrimLeft(Str, " ")
+		fmt.Println(Str)
+
+	}
 
 	return n, err
+}
+func WriteData(a ...any) {
+
+	file, err := os.Create(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	// сохраняем данные в файл
+	fmt.Fprint(file, a)
 }
 
 func Println(a ...any) (n int, err error) {
